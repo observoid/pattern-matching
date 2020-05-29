0a0a44f69f76df2709d946ed591b9d521d5745c6
Duncan Cross <duncan.cross@gmail.com>
2020-05-29 14:09:40 +0100

    Rename captureInput to captureAllInput and add matchAnyInput

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 
-import { Observable, OperatorFunction, empty } from 'rxjs';
+import { Observable, OperatorFunction, empty, ReplaySubject, Subscription } from 'rxjs';
 
 export interface CaptureValue<TCapture> {
   complete?: false;
@@ -13,9 +13,9 @@ export interface CaptureComplete<TInput> {
 
 export type Capture<TInput, TCapture> = CaptureValue<TCapture> | CaptureComplete<TInput>;
 
-export function captureInput<TInput>(): OperatorFunction<TInput, Capture<TInput, TInput>> {
+export function captureAllInput<TInput>(): OperatorFunction<TInput, Capture<TInput, TInput>> {
   return input => new Observable(subscriber => {
-    input.subscribe(
+    return input.subscribe(
       capture => {
         subscriber.next({capture});
       },
@@ -25,5 +25,32 @@ export function captureInput<TInput>(): OperatorFunction<TInput, Capture<TInput,
       () => {
         subscriber.next({complete: true, suffix: empty()});
       });
+  });
+}
+
+export interface Match<TInput, TMatch> {
+  match: TMatch;
+  suffix: Observable<TInput>;
+}
+
+export function matchAnyInput<TInput>(): OperatorFunction<TInput, Match<TInput, TInput>> {
+  return input => new Observable(subscriber => {
+    const subscription = new Subscription();
+    let onComplete = () => subscriber.complete();
+    let onError = (error: any) => subscriber.error(error);
+    let onInput = (match: TInput) => {
+      const suffix = new ReplaySubject<TInput>();
+      subscription.add(suffix);
+      onInput = match => suffix.next(match);
+      onComplete = () => suffix.complete();
+      onError = (error) => suffix.error(error);
+      subscriber.next({match, suffix});
+      subscriber.complete();
+    };
+    subscription.add(input.subscribe(
+      input => onInput(input),
+      error => onError(error),
+      () => onComplete()));
+    return subscription;
   });
 }
