f9c4428ab26405e6b49106d58f3e523c08def0dc
Duncan Cross <duncan.cross@gmail.com>
2020-05-29 16:21:30 +0100

    Add test for mapCaptures

--- src/__tests__/captures.ts
+++ src/__tests__/captures.ts
@@ -1,9 +1,9 @@
 
-import { captureAllInput, CaptureValue, CaptureComplete } from '../index'
+import { captureAllInput, CaptureValue, CaptureComplete, mapCaptures } from '../index'
 import { from, Observable } from 'rxjs';
 import { toArray } from 'rxjs/operators';
 
-test('captureAllInput works', done => {
+test('captureAllInput', done => {
   const testValues = ['banana', 33, true];
   from(testValues)
   .pipe(
@@ -17,6 +17,43 @@ test('captureAllInput works', done => {
         let obj = (val[i] || {}) as CaptureValue<any>;
         expect(obj.complete).toBeFalsy();
         expect(obj.capture).toBe(testValues[i]);
+      }
+      let last = (val[testValues.length] || {}) as CaptureComplete<any>;
+      expect(last.complete).toBe(true);
+      expect(last.suffix).toBeInstanceOf(Observable);
+      last.suffix.pipe( toArray() ).subscribe({
+        next(val) {
+          expect(val).toHaveLength(0);
+        },
+        complete() {
+          done();
+        },
+        error(e) {
+          done(e);
+        },
+      });
+    },
+    error(e) {
+      done(e);
+    },
+  });
+});
+
+test('mapCaptures', done => {
+  const testValues = [1, 2, 3];
+  from(testValues)
+  .pipe(
+    captureAllInput(),
+    mapCaptures(v => v * 100),
+    toArray()
+  )
+  .subscribe({
+    next(val) {
+      expect(val).toHaveLength(testValues.length + 1);
+      for (let i = 0; i < testValues.length; i++) {
+        let obj = (val[i] || {}) as CaptureValue<any>;
+        expect(obj.complete).toBeFalsy();
+        expect(obj.capture).toBe(testValues[i] * 100);
       }
       let last = (val[testValues.length] || {}) as CaptureComplete<any>;
       expect(last.complete).toBe(true);
